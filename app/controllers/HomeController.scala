fc4629ec1d8b7fb5ee46b923875a74ca8d4ddb97
Teena <teena@knoldus.com>
2016-10-06 23:05:01 +0530

    removed compilation error

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,14 +1,9 @@
 package controllers
 
 import javax.inject._
-import play.api._
-import play.api.mvc._
-import play.api.libs.json._
 
-/**
-  * This controller creates an `Action` to handle HTTP requests to the
-  * application's home page.
-  */
+import play.api.libs.json._
+import play.api.mvc._
 
 case class User(firstName: String,
                 lastName: String,
@@ -18,16 +13,17 @@ case class Location(lat: Double, long: Double)
 
 class HomeController @Inject() extends Controller {
 
-  /**
-    * Create an Action to render an HTML page with a welcome message.
-    * The configuration in the `routes` file means that this method
-    * will be called when the application receives a `GET` request with
-    * a path of `/`.
-    */
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def example = Action {
+    Ok(views.html.user("example1"))
+  }
+
+  def observableExample = Action{
+    Ok(views.html.observableExample("example2"))
+  }
 
   def userDetail = Action {
     implicit val userInfo = new Writes[User] {
